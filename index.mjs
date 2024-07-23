6439c6145bb3dbceb4943cb9d4588ce4e5bf810c
Cerberus <shadow_sun0708@hotmail.com>
2024-07-24 01:33:07 +0700

    initial commit

---
+++ index.mjs
@@ -0,0 +1,46 @@
+import { readdir } from 'node:fs/promises'
+import sharp from 'sharp'
+import { execSync } from 'child_process'
+
+const scanDir = '../'
+const directs = await readdir(scanDir, {
+	recursive: true,
+	withFileTypes: true,
+})
+const files = directs.filter(
+	(direct) =>
+		direct.isFile() &&
+		['png', 'jpg', 'jpeg'].some((ext) => direct.name.endsWith(ext)),
+)
+
+sharp.cache(false)
+
+const outputDirIdx = {}
+await files
+	.reduce(async (acc, file) => {
+		await acc
+		const parentDir = file.parentPath ?? '.'
+		const filePath = `${parentDir}/${file.name}`
+
+		const outputName = 'output'
+		execSync(`mkdir -p "${parentDir}/${outputName}"`)
+		outputDirIdx[`${parentDir}/${outputName}`] = true
+
+		console.log('exec', filePath)
+		return sharp(`./${filePath}`)
+			.toFormat('jpg', { lossless: true, mozjpeg: true })
+			.toFile(`${file.parentPath ?? '.'}/${outputName}/${file.name}`)
+			.then(() => {
+				execSync(
+					`mv -f "${file.parentPath ?? '.'}/${outputName}/${
+						file.name
+					}" "${filePath}"`,
+				)
+			})
+	}, Promise.resolve())
+	.finally(() => {
+		Object.keys(outputDirIdx).forEach((outputDir) => {
+			console.log('rm', outputDir)
+			execSync(`rm -rf "${outputDir}"`)
+		})
+	})
